29ca61e8a3dd90931f31c923de029f7b0560fc85
MagnusValtersson <magnus.valtersson@gmail.com>
2025-12-04 08:13:17 +0100

    Day4 complete

---
+++ day4.py
@@ -0,0 +1,57 @@
+from AdventDay import AdventDay
+
+class Day4():
+    def __init__(self):
+        self.day = AdventDay("Day4", "input/day4.txt")
+        self.answer = 0
+        self.data = []
+        i = 0
+        for line in self.day.file:
+            self.data.append(list(line))
+            i += 1
+
+    def getNeighbors(self, row, col):
+        return [[row-1, col-1], [row-1, col], [row-1, col+1], [row, col-1], [row, col+1], [row+1, col-1], [row+1, col], [row+1, col+1]]
+    
+    def getIfPaperRoll(self, row, col):
+        if row < 0 or col < 0 or row >= len(self.data) or col >= len(self.data[0]):
+            return False
+        if self.data[row][col] == '@':
+            return True
+        else:
+            return False
+
+    def getAnswer(self):
+        print("Calculating Day3")
+        self.calculateAnswerB()
+        print("Done")
+        return str(self.answer)
+    
+    def calculateAnswerB(self):
+        managedToMove = True
+        while managedToMove:
+            managedToMove = False
+            for row in range(0, len(self.data)):
+                for col in range(0, len(self.data[0])):
+                    numOfPaperRolls = 0
+                    if self.getIfPaperRoll(row, col):
+                        neighbors = self.getNeighbors(row, col)
+                        for r, c in neighbors:
+                            if self.getIfPaperRoll(r, c):
+                                numOfPaperRolls += 1
+                        if numOfPaperRolls < 4:
+                            self.data[row][col] = '.'
+                            self.answer += 1
+                            managedToMove = True
+    
+    def calculateAnswerA(self):
+        for row in range(0, len(self.data)):
+            for col in range(0, len(self.data[0])):
+                numOfPaperRolls = 0
+                if self.getIfPaperRoll(row, col):
+                    neighbors = self.getNeighbors(row, col)
+                    for r, c in neighbors:
+                        if self.getIfPaperRoll(r, c):
+                            numOfPaperRolls += 1
+                    if numOfPaperRolls < 4:
+                        self.answer += 1
--- mainMenu.py
+++ mainMenu.py
@@ -1,9 +1,10 @@
 from tkinter import *
 from functools import partial
 import importlib
-from day1 import Day1
-from day2 import Day2
-from day3 import Day3
+import day1
+import day2
+import day3
+import day4
 
 class MainMenu():
     def __init__(self, title, geometry):
@@ -23,13 +24,18 @@ class MainMenu():
         self.buttons[name] = Button(self.root, text=name, anchor="center", cursor="hand2", pady=5, width=4, height=1, command = partial(menu.insertText, day))
 
     def insertText(self, dayNr):
+        importlib.reload(day1)
+        importlib.reload(day2)
+        importlib.reload(day3)
+        importlib.reload(day4)
         day = dayNr()
         self.textBox.delete('1.0', END)
         self.textBox.insert(END, day.getAnswer() + '\n')
 
 if __name__ == "__main__":
     menu = MainMenu("Advent of Code 2025", "600x600")
-    menu.addButton("Day1", Day1)
-    menu.addButton("Day2", Day2)
-    menu.addButton("Day3", Day3)
+    menu.addButton("Day1", day1.Day1)
+    menu.addButton("Day2", day2.Day2)
+    menu.addButton("Day3", day3.Day3)
+    menu.addButton("Day4", day4.Day4)
     menu.finalize()
